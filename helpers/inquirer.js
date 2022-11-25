884bb5b74bdfe0d1655f0f00dc309ddeb534e6aa
Diego Palacio <palaciodiego099@gmail.com>
2022-11-25 15:32:34 -0500

    CLIMATE-APP v0.0

---
+++ helpers/inquirer.js
@@ -0,0 +1,133 @@
+import inquirer from 'inquirer';
+import colors from 'colors';
+
+const questions = [
+    {
+        type: 'list',
+        name: 'option',
+        message: 'What do you want to do?',
+        choices: [
+            {
+                value: 1,
+                name: `${'1.'.green} Search city`
+            },
+            {
+                value: 2,
+                name: `${'2.'.green} History`
+            },
+            {
+                value: 0,
+                name: `${'0.'.green} Exit`
+            },
+
+        ]
+    }
+];
+
+
+
+const inquirerMenu = async () => {
+
+    console.log('=========================='.green);
+    console.log('  Select an option'.white);
+    console.log('==========================\n'.green);
+
+    const { option } = await inquirer.prompt(questions);
+
+    return option;
+}
+
+const pause = async () => {
+
+    const question = [
+        {
+            type: 'input',
+            name: 'enter',
+            message: `Press ${'enter'.green} to continue`
+        }
+    ];
+
+    console.log('\n');
+    await inquirer.prompt(question);
+}
+
+
+
+const readInput = async (message) => {
+    const question = [
+        {
+            type: 'input',
+            name: 'desc',
+            message,
+            validate(value) {
+                if (value.length === 0) {
+                    return 'Please enter a value';
+                }
+                return true;
+            }
+        },
+    ]
+    const { desc } = await inquirer.prompt(question);
+    return desc;
+}
+
+
+
+const confirm = async (message) => {
+
+
+    const question = [
+        {
+            type: 'confirm',
+            name: 'ok',
+            message
+        }
+    ];
+
+    const { ok } = await inquirer.prompt(question);
+    return ok;
+}
+
+
+const listPlaces = async (places = []) => {
+    
+        const choices = places.map((place, i) => {
+    
+            const idx = `${i + 1}.`.green;
+    
+            return {
+                value: place.id,
+                name: `${idx} ${place.name}`
+            }
+        });
+    
+        choices.unshift({
+            value: 0,
+            name: '0.'.green + ' Cancel'
+        });
+    
+        const questions = [
+            {
+                type: 'list',
+                name: 'id',
+                message: 'Select place',
+                choices
+            }
+        ];
+    
+        const { id } = await inquirer.prompt(questions);
+        return id;
+}
+
+
+
+
+
+
+export {
+    inquirerMenu,
+    pause,
+    readInput,
+    confirm,
+    listPlaces
+}
---
+++ models/searches.js
@@ -0,0 +1,104 @@
+import fetch from 'node-fetch'
+import fs from 'fs'
+
+
+class Searches {
+    history = [];
+    dbPath = './db/database.json';
+    constructor() {
+        this.readDB();
+    }
+
+    get historyCapitalized () {
+        return this.history.map(place => {
+            let words = place.split(' ');
+            words = words.map(p => p[0].toUpperCase() + p.substring(1));
+
+            return words.join(' '); 
+        })
+    }
+
+    
+    async city(place = '') {
+
+        try {
+        
+             const res = await fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json?types=place&language=es%2Cen&access_token=${process.env.MAPBOX_KEY}`)
+             const data = await res.json();
+        
+                return data.features.map(place => ({
+                    id: place.id,
+                    name: place.place_name,
+                    lng: place.center[0],
+                    lat: place.center[1]
+
+                }))
+
+        } catch (error) {
+            return [];
+        }
+
+    }
+
+ 
+ async weatherPlacer(lag, lgn) {
+    try {
+        const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lag}&lon=${lgn}&appid=${process.env.OPENWEATHER_KEY}&units=metric`)
+        const data = await res.json();
+
+        let dt = {
+            desc: data.weather[0].description,
+            min: data.main.temp_min,
+            max: data.main.temp_max,
+            temp: data.main.temp
+        
+        }
+        return dt;
+
+    } catch (error) {
+        return console.log(error);
+    }
+ }
+
+ async addHistory(place = '')  {
+    if (this.history.includes(place.toLocaleLowerCase())) {
+        return;
+    }
+
+    this.history = this.history.splice(0, 5);
+
+    
+    
+    this.history.unshift(place.toLocaleLowerCase());
+
+    
+    this.saveDB();
+
+ }
+
+ async saveDB() {
+    const payload = {
+        history: this.history
+    }
+
+    fs.writeFileSync(this.dbPath, JSON.stringify(payload));
+ }
+
+    async readDB() {
+        if (!fs.existsSync(this.dbPath)) {
+            return;
+        }
+
+        const info = fs.readFileSync(this.dbPath, {encoding: 'utf-8'});
+        const data = JSON.parse(info);
+        this.history = data.history;
+    }
+
+
+
+}
+
+export default Searches;
+
+
+
